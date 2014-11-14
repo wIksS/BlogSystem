1399e1dc21b006fe6df5976936c5d131fe3c81a1
TCVD <viktordakov97@gmail.com>
2014-11-14 14:15:07 +0200

    Many changes to the layout page.

--- BlogSystem.Data/BlogSystemDbContext.cs
+++ BlogSystem.Data/BlogSystemDbContext.cs
@@ -24,6 +24,8 @@ namespace BlogSystem.Data
 
         public IDbSet<Page> Pages { get; set; }
 
+        public System.Data.Entity.DbSet<BlogSystem.Models.BlogPost> BlogPosts { get; set; }
+
        // public IDbSet<ApplicationUser> Users { get; set; }
     }
 }
---
+++ BlogSystem.Web/Areas/Administration/Controllers/BlogPostsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogSystem.Data;
+using BlogSystem.Models;
+
+namespace BlogSystem.Web.Areas.Administration.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private BlogSystemDbContext db = new BlogSystemDbContext();
+
+        // GET: Administration/BlogPosts
+        public ActionResult Index()
+        {
+            var blogPosts = db.BlogPosts.Include(b => b.Category);
+            return View(blogPosts.ToList());
+        }
+
+        // GET: Administration/BlogPosts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // GET: Administration/BlogPosts/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Title");
+            return View();
+        }
+
+        // POST: Administration/BlogPosts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Content,CategoryId,ApplicationUserId")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BlogPosts.Add(blogPost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Title", blogPost.CategoryId);
+            return View(blogPost);
+        }
+
+        // GET: Administration/BlogPosts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Title", blogPost.CategoryId);
+            return View(blogPost);
+        }
+
+        // POST: Administration/BlogPosts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Content,CategoryId,ApplicationUserId")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogPost).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Title", blogPost.CategoryId);
+            return View(blogPost);
+        }
+
+        // GET: Administration/BlogPosts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            if (blogPost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogPost);
+        }
+
+        // POST: Administration/BlogPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BlogPost blogPost = db.BlogPosts.Find(id);
+            db.BlogPosts.Remove(blogPost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
